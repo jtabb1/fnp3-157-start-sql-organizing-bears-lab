38edd7580f26cc2a8b7105525b43f2c7a7995d64
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-18 16:12:51 -0400

    test athe different official solution

--- lib/create.sql
+++ lib/create.sql
@@ -1,3 +1,4 @@
+-- the original solution passes all tests
 CREATE TABLE bears (
 id INTEGER PRIMARY KEY,
 name TEXT,
@@ -7,3 +8,16 @@ color TEXT,
 temperament TEXT,
 alive INTEGER
 );
+
+-- the official solution works also:
+/*
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex CHAR(1),
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+*/
